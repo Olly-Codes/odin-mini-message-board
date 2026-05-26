92588d8c9856e43b04c6211edcf3b0b9c9c922ba
Olly <ollycodesdev@gmail.com>
2026-05-26 16:49:48 +0200

    feat: add form for new messages

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) => res.send("This is the messages index"));
+messagesRouter.get("/", (req, res) => {
+    res.render("form");
+});
 
 module.exports = messagesRouter;
