1355905101dc028dceb0101932a15edff00c9f8f
Olly <ollycodesdev@gmail.com>
2026-05-26 18:01:56 +0200

    feat: add public folder and linking styles to views

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const { indexRouter } = require("./routes/indexRouter");
 const messagesRouter = require("./routes/messagesRouter");
 const detailsRouter = require("./routes/detailsRouter");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
