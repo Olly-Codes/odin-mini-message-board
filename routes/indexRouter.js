cb34c1948a3254d0f4967caae91b31efd0a8f9de
Olly <ollycodesdev@gmail.com>
2026-05-26 16:31:53 +0200

    feat: add messages array for rendering

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,21 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("This is the index"));
+const messages = [
+    {
+        text: "Huh, is that the wizard of time?",
+        user: "TheCurious",
+        added: new Date()
+    },
+    {
+        text: "Who knows, maybe I am, maybe I am not. Only time will tell",
+        user: "DefinitelyNotTWOT",
+        added: new Date()
+    },
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", { messages: messages});
+});
 
 module.exports = indexRouter;
