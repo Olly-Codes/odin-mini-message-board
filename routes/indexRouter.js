140f69612a6bc350dc05bb2ef68f502e05ccd099
Olly <ollycodesdev@gmail.com>
2026-05-30 15:38:51 +0200

    feat: add post route for deletion of messages and dedicated controller function

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,6 @@ const messagesController = require("../controllers/messagesController");
 const indexRouter = Router();
 
 indexRouter.get("/", messagesController.messagesListGet);
+indexRouter.post("/delete/:messageId", messagesController.messageDelete);
 
 module.exports = indexRouter;
