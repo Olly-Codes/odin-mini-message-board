af0d951454cba1acd2fc860914d6590fad2d44f5
Olly <ollycodesdev@gmail.com>
2026-05-30 14:04:00 +0200

    refactor: change where logic is handled and linked router to dedicated controller function

--- routes/detailsRouter.js
+++ routes/detailsRouter.js
@@ -1,12 +1,8 @@
 const { Router } = require("express");
-const { messages } = require("./indexRouter");
+const messagesController = require("../controllers/messagesController");
 
 const detailsRouter = Router();
 
-detailsRouter.get("/:messageId", (req, res) => {
-    const id = req.params.messageId;
-    const message = messages.find((msg) => msg.id === id);
-    res.render("details", { title: "Details", message: message });
-});
+detailsRouter.get("/:messageId", messagesController.messageGet);
 
 module.exports = detailsRouter;
