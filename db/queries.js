176c0df5c732092d3bc56c9c834958371d11c2b3
Olly <ollycodesdev@gmail.com>
2026-05-30 14:56:02 +0200

    fix: user id was being fetched, changed to message id

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 const getAllMessages = async () => {
     const { rows } = await pool.query(
-        `SELECT u.id, u.username, m.message_content, m.date_added
+        `SELECT m.id, u.username, m.message_content, m.date_added
         FROM users u JOIN messages m
         ON u.id = m.user_id;
         `
