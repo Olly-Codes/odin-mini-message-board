dfacec67baadd4a2ed37598e8eb0b024b822fb70
Olly <ollycodesdev@gmail.com>
2026-05-30 14:59:30 +0200

    refactor: moved new messages handling to messages router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,37 +3,6 @@ const messagesController = require("../controllers/messagesController");
 
 const indexRouter = Router();
 
-// const messages = [
-//     {
-//         text: "Huh, is that the wizard of time?",
-//         user: "TheCurious",
-//         added: new Date().toDateString(),
-//         id: crypto.randomUUID()
-//     },
-//     {
-//         text: "Who knows, maybe I am, maybe I am not. Only time will tell",
-//         user: "DefinitelyNotTWOT",
-//         added: new Date().toDateString(),
-//         id: crypto.randomUUID()
-//     },
-// ];
-
 indexRouter.get("/", messagesController.messagesListGet);
-
-// indexRouter.post("/new", (req, res) => {
-//     const messageUser = req.body.user;
-//     const messageText = req.body.messageText;
-
-//     messages.push(
-//         {
-//             text: messageText,
-//             user: messageUser,
-//             added: new Date().toDateString(),
-//             id: crypto.randomUUID()
-//         }
-//     );
-
-//     res.redirect("/");
-// });
 
 module.exports = indexRouter;
