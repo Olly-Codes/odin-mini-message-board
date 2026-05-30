97db7c88fef33a0738b79b268cfc45c2de287a47
Olly <ollycodesdev@gmail.com>
2026-05-30 13:24:20 +0200

    feat: import and handle index route to messages controller

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,40 +1,39 @@
 const { Router } = require("express");
+const messagesController = require("../controllers/messagesController");
 
 const indexRouter = Router();
 
-const messages = [
-    {
-        text: "Huh, is that the wizard of time?",
-        user: "TheCurious",
-        added: new Date().toDateString(),
-        id: crypto.randomUUID()
-    },
-    {
-        text: "Who knows, maybe I am, maybe I am not. Only time will tell",
-        user: "DefinitelyNotTWOT",
-        added: new Date().toDateString(),
-        id: crypto.randomUUID()
-    },
-];
+// const messages = [
+//     {
+//         text: "Huh, is that the wizard of time?",
+//         user: "TheCurious",
+//         added: new Date().toDateString(),
+//         id: crypto.randomUUID()
+//     },
+//     {
+//         text: "Who knows, maybe I am, maybe I am not. Only time will tell",
+//         user: "DefinitelyNotTWOT",
+//         added: new Date().toDateString(),
+//         id: crypto.randomUUID()
+//     },
+// ];
 
-indexRouter.get("/", (req, res) => {
-    res.render("index", { title: "Home", messages: messages});
-});
+indexRouter.get("/", messagesController.messagesListGet);
 
-indexRouter.post("/new", (req, res) => {
-    const messageUser = req.body.user;
-    const messageText = req.body.messageText;
+// indexRouter.post("/new", (req, res) => {
+//     const messageUser = req.body.user;
+//     const messageText = req.body.messageText;
 
-    messages.push(
-        {
-            text: messageText,
-            user: messageUser,
-            added: new Date().toDateString(),
-            id: crypto.randomUUID()
-        }
-    );
+//     messages.push(
+//         {
+//             text: messageText,
+//             user: messageUser,
+//             added: new Date().toDateString(),
+//             id: crypto.randomUUID()
+//         }
+//     );
 
-    res.redirect("/");
-});
+//     res.redirect("/");
+// });
 
-module.exports = { indexRouter, messages };
+module.exports = indexRouter;
