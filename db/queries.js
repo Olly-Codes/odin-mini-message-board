f52b3182ea57d6ddbf3848fa558214173231725c
Olly <ollycodesdev@gmail.com>
2026-05-30 14:01:51 +0200

    feat: add query to fetch details about a specific message

--- db/queries.js
+++ db/queries.js
@@ -10,6 +10,18 @@ const getAllMessages = async () => {
     return rows;
 }
 
+const getMessage = async (message_id) => {
+    const query =`
+        SELECT u.username, m.message_content, m.date_added
+        FROM users u JOIN messages m
+        ON u.id = m.user_id
+        WHERE m.id = $1;
+    `;
+    const { rows } = await pool.query(query, [message_id]);
+    return rows[0];
+}
+
 module.exports = {
-    getAllMessages
+    getAllMessages,
+    getMessage
 }
