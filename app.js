9c54bc4b5114e5cc00c1725eb03a403809b1df93
Olly <ollycodesdev@gmail.com>
2026-05-30 13:23:28 +0200

    feat: add dedicated indexRouter

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("node:path");
 const app = express();
-const { indexRouter } = require("./routes/indexRouter");
-const messagesRouter = require("./routes/messagesRouter");
-const detailsRouter = require("./routes/detailsRouter");
+const indexRouter = require("./routes/indexRouter");
+// const messagesRouter = require("./routes/messagesRouter");
+// const detailsRouter = require("./routes/detailsRouter");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
@@ -14,8 +14,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
-app.use("/new", messagesRouter);
-app.use("/details", detailsRouter);
+// app.use("/new", messagesRouter);
+// app.use("/details", detailsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
